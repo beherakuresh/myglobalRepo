501eb813603bae00eecbe41dcf61d90c49195eab
kuresh1234 <110728330+kuresh1234@users.noreply.github.com>
2023-08-17 11:16:02 +0530

    hy

---
+++ Mypractice/src/test/java/CreateAccount.java
@@ -0,0 +1,19 @@
+import org.testng.annotations.Test;
+
+public class CreateAccount {
+	@Test
+	public void accountCreation()
+	{
+		
+	}
+	@Test
+	public void accountDelete()
+	{
+		
+	}
+	@Test
+	public void accountUpdated()
+	{
+		
+	}
+}
